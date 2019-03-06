c11f167bee78214b501f6b4c63b72266d2959ca7
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-03-06 14:10:12 +0100

    Log with morgan in dev mode

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 const port = process.env.PORT || 5000
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'))
+}
 
 app.get('/', (req, res) => {
   res.send({message: 'Hello world!'})
