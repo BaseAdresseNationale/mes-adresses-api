623f68a25d7366b85d1c6813b409612c70f93b92
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-03-06 14:17:39 +0100

    Allow CORS requests (default config)

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 5000
+
+app.use(cors())
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'))
