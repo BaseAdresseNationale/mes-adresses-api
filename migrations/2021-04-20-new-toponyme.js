bcdddbc9205e97be8b35f2b99bc5f1c74b0665c9
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-04-22 18:10:00 +0200

    Rewrite migration

--- migrations/2021-04-20-new-toponyme.js
+++ migrations/2021-04-20-new-toponyme.js
@@ -1,106 +1,101 @@
 #!/usr/bin/env node
+/* eslint no-await-in-loop: off */
 require('dotenv').config()
-const {groupBy, uniq, flatten} = require('lodash')
+const {keyBy, deburr} = require('lodash')
 const mongo = require('../lib/util/mongo')
 const {ObjectID} = require('../lib/util/mongo')
-const {normalizeString} = require('../lib/populate/extract-csv')
 
-// Regroupe les toponymes et complément de voie sous un identifiant unique
-async function groupToponymes() {
-  // Récupères toutes les voies avec des positions, considéré comme des toponymes
-  const toponymes = await mongo.db.collection('voies').find({positions: {$ne: []}}).toArray()
+const now = new Date()
+
+function normalizeString(string) {
+  return deburr(string.trim().toLowerCase())
+}
+
+async function runMigrationOnContext({_bal, commune}) {
+  // On récupère toutes les voies avec des positions, considérées actuellement comme des toponymes
+  const voiesToponymes = await mongo.db.collection('voies').find({_bal, commune, positions: {$ne: []}}).toArray()
+
+  // On construit les nouveaux toponymes à partir des anciennes voies/toponymes
+  const toponymes = voiesToponymes.map(voieToponyme => ({
+    _bal,
+    commune,
+    _id: new ObjectID(),
+    nom: voieToponyme.nom,
+    positions: voieToponyme.positions,
+    _created: voieToponyme._created,
+    _updated: voieToponyme._updated
+  }))
+
+  // On créé un index selon le libellé normalisé, qui nous servira à rechercher à partir du complément
+  const toponymesIndex = keyBy(toponymes, t => normalizeString(t.nom))
+
   // Récupères toutes les voies avec un complément, ce complément est considéré comme un toponyme
-  const complements = await mongo.db.collection('voies').find({complement: {$ne: null}}).toArray()
+  const voiesAvecComplement = await mongo.db.collection('voies').find({_bal, commune, complement: {$ne: null}}).toArray()
 
-  return groupBy([...toponymes, ...complements], v => {
-    const nom = normalizeString(v.complement || v.nom)
-    return `${v._bal}_${v.commune}_${nom}`
-  })
-}
+  // Pour chacun de ces voies on se rattache à un toponyme existant ou on en créé un
+  for (const voie of voiesAvecComplement) {
+    // On normalise le libellé du complément
+    const normalizedComplement = normalizeString(voie.complement)
 
-async function mergeToponymes(toponymesGroups) {
-  return Promise.all(Object.keys(toponymesGroups).map(async key => {
-    const voies = toponymesGroups[key]
+    // Si on a pas déjà ce toponyme, on le créé et on l'ajoute à l'index
+    if (!(normalizedComplement in toponymesIndex)) {
+      const toponyme = {
+        _bal,
+        commune,
+        _id: new ObjectID(),
+        nom: voie.complement,
+        positions: [],
+        _created: now,
+        _updated: now
+      }
 
-    // Crée un tableau contenant la liste des id des voies avec le même complément
-    const voiesIds = voies.map(({_id}) => mongo.parseObjectID(_id))
-
-    // Récupère tous les numéros des voies ayant le même complément
-    const numeros = await mongo.db.collection('numeros').find({voie: {$in: voiesIds}}).toArray()
-
-    // Extrait la BAL, la commune et le complement du groupe
-    const {_bal, commune, complement, nom} = voies[0]
-    const nomToponyme = complement || nom // Améliorer en récupérer le string avec le plus d'occurence
-
-    return {
-      nomToponyme,
-      numeros,
-      positions: uniq(flatten(voies.map(({positions}) => positions))),
-      commune,
-      _bal
+      toponymes.push(toponyme)
+      toponymesIndex[normalizedComplement] = toponyme
     }
-  }))
-}
 
-async function migrateToponymes() {
-  // Regroupe toutes les "types" de toponyme
-  const toponymesGroups = await groupToponymes()
+    // On récupère le toponyme correspondant (qu'on l'ait créé juste avant ou pas)
+    const toponymeId = toponymesIndex[normalizedComplement]._id
 
-  // Fusionne tous les toponymes identique
-  const mergedtoponymes = await mergeToponymes(toponymesGroups)
+    // On migre les numéros de la voie en ajoutant la référence au toponyme
+    await mongo.db.collection('numeros').updateMany({voie: voie._id}, {$set: {toponyme: toponymeId}})
+  }
 
-  // Attribu un ID aux toponymes et associe leurs numéros
-  const toponymes = await Promise.all(mergedtoponymes.map(async t => {
-    const {nom, positions, commune, _bal, _created, _updated} = t
-    const idToponyme = new ObjectID()
+  // S'il n'y a pas de toponymes dans la liste, pas la peine de continuer
+  if (toponymes.length === 0) {
+    return
+  }
 
-    // Associe les numéros à leur toponyme
-    await Promise.all(t.numeros.map(async numero => {
-      await mongo.db.collection('numeros').findOneAndUpdate(
-        {_id: numero._id},
-        {$set: {toponyme: idToponyme}}
-      )
-    }))
-
-    return {
-      _id: idToponyme,
-      nom,
-      _bal,
-      commune,
-      positions,
-      _created,
-      _updated
-    }
-  }))
-
-  // Création des toponymes avec les éléments utiles issues des voies
+  // On insert les toponymes qu'on a créé dans la nouvelle collection de la base de données
   await mongo.db.collection('toponymes').insertMany(toponymes)
 
-  // Supprime les voies qui ont étaient converties en toponyme
-  const oldVoiesToponymes = await mongo.db.collection('voies').find({positions: {$ne: []}}).toArray()
-  await mongo.db.collection('voies').deleteMany({
-    _id: {
-      $in: oldVoiesToponymes.map(({_id}) => _id)
-    }
-  })
+  // On supprime les voies qui étaient considérées comme des toponymes
+  await mongo.db.collection('voies').deleteMany({_bal, commune, positions: {$ne: []}})
 
-  // Supprime les champs 'complement' et 'positions' de voies, désormais inutile
-  await mongo.db.collection('voies').updateMany({},
-    {$unset: {complement: 1, positions: 1}}
-  )
+  // On supprime le champ complement
+  await mongo.db.collection('voies').updateMany({_bal, commune}, {$unset: {complement: 1}})
+}
+
+async function listContexts() {
+  const result = await mongo.db.collection('bases_locales').aggregate([
+    {$unwind: '$communes'}
+  ]).toArray()
+
+  return result.map(({_id, communes}) => ({_bal: _id, commune: communes}))
 }
 
 async function main() {
-  if (process.env.RUN_MIGRATION === 1) {
-    await mongo.connect()
-    await migrateToponymes()
-    await mongo.disconnect()
+  await mongo.connect()
+
+  const contexts = await listContexts()
+
+  for (const context of contexts) {
+    await runMigrationOnContext(context)
   }
+
+  await mongo.disconnect()
 }
 
 main().catch(error => {
   console.error(error)
   process.exit(1)
 })
-
-module.exports = {migrateToponymes, groupToponymes, mergeToponymes}
