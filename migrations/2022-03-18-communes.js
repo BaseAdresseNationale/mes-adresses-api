069a6e5fe00aac7fea842400cfab976fc820ddb5
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2022-03-18 12:12:02 +0400

    Create migration script to replace communes by commune

---
+++ migrations/2022-03-18-communes.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+require('dotenv').config()
+const mongo = require('../lib/util/mongo')
+
+async function main() {
+  await mongo.connect()
+
+  await mongo.db.collection('bases_locales').updateMany(
+    {},
+    [
+      {$set: {commune: {$first: '$communes'}}},
+      {$unset: ['communes']}
+    ]
+  )
+
+  await mongo.disconnect()
+}
+
+main().catch(error => {
+  console.log(error)
+  process.exit(1)
+})
