3784c9f31c4c66b46bc315a4070a3c3b82ced946
Cédric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2022-04-07 15:42:22 +0400

    Adapt migration script to mongo previous version

--- migrations/2022-03-18-communes.js
+++ migrations/2022-03-18-communes.js
@@ -1,20 +1,31 @@
 #!/usr/bin/env node
-
+/* eslint no-await-in-loop: off */
 require('dotenv').config()
 const mongo = require('../lib/util/mongo')
+
+async function runMigrationOnContext({_bal, commune}) {
+  await mongo.db.collection('bases_locales').updateOne({_id: _bal}, {$set: {commune}, $unset: {communes: 1}})
+}
+
+async function listContexts() {
+  const result = await mongo.db.collection('bases_locales').aggregate([
+    {$addFields: {commune: {$first: '$communes'}}}
+  ]
+  ).toArray()
+
+  return result.map(({_id, commune}) => ({_bal: _id, commune}))
+}
 
 async function main() {
   await mongo.connect()
 
   await mongo.db.collection('bases_locales').deleteMany({communes: {$eq: []}})
 
-  await mongo.db.collection('bases_locales').updateMany(
-    {},
-    [
-      {$set: {commune: {$first: '$communes'}}},
-      {$unset: ['communes']}
-    ]
-  )
+  const contexts = await listContexts()
+
+  for (const context of contexts) {
+    await runMigrationOnContext(context)
+  }
 
   await mongo.disconnect()
 }
