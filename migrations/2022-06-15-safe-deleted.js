1b313a9f0d89e23d6bcd9938fc7d8b9edfecd598
gllmr <guillaume@livingdata.co>
2022-06-17 17:40:45 +0200

    Update migration script

--- migrations/2022-06-15-safe-deleted.js
+++ migrations/2022-06-15-safe-deleted.js
@@ -6,9 +6,15 @@ const mongo = require('../lib/util/mongo')
 async function main() {
   await mongo.connect()
 
-  let counter = 0
+  let counterBal = 0
+  let counterNum = 0
+  let counterTopo = 0
+  let counterVoie = 0
 
   const balCursor = await mongo.db.collection('bases_locales').find({_deleted: {$exists: false}})
+  const numerosCursor = await mongo.db.collection('numeros').find({_deleted: {$exists: false}})
+  const toponymesCursor = await mongo.db.collection('toponymes').find({_deleted: {$exists: false}})
+  const voiesCursor = await mongo.db.collection('voies').find({_deleted: {$exists: false}})
 
   while (await balCursor.hasNext()) {
     const baseLocale = await balCursor.next()
@@ -18,13 +24,49 @@ async function main() {
       {$set: {_deleted: null}}
     )
 
-    counter += 1
+    counterBal += 1
+  }
+
+  while (await numerosCursor.hasNext()) {
+    const numero = await numerosCursor.next()
+
+    await mongo.db.collection('numeros').updateOne(
+      {_id: numero._id},
+      {$set: {_deleted: null}}
+    )
+
+    counterNum += 1
+  }
+
+  while (await toponymesCursor.hasNext()) {
+    const toponyme = await toponymesCursor.next()
+
+    await mongo.db.collection('toponymes').updateOne(
+      {_id: toponyme._id},
+      {$set: {_deleted: null}}
+    )
+
+    counterTopo += 1
+  }
+
+  while (await voiesCursor.hasNext()) {
+    const voie = await voiesCursor.next()
+
+    await mongo.db.collection('voies').updateOne(
+      {_id: voie._id},
+      {$set: {_deleted: null}}
+    )
+
+    counterVoie += 1
   }
 
   console.log('\nTerminé !\n')
-  console.log('****************************\n')
-  console.log(` ${counter} BAL ont été modifiées \n`)
-  console.log('****************************\n')
+  console.log('************************************\n')
+  console.log(` ${counterBal} BAL ont été modifiées \n`)
+  console.log(` ${counterNum} numéros ont été modifiés \n`)
+  console.log(` ${counterTopo} toponymes ont été modifiés \n`)
+  console.log(` ${counterVoie} voies ont été modifiées \n`)
+  console.log('************************************\n')
 
   await mongo.disconnect()
   process.exit(1)
