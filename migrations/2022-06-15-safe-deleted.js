5e6b4862bd166ff046447fe37bdeb0aa8ea118d9
gllmr <guillaume@livingdata.co>
2022-06-23 14:38:37 +0200

    Simplify migration script

--- migrations/2022-06-15-safe-deleted.js
+++ migrations/2022-06-15-safe-deleted.js
@@ -6,66 +6,32 @@ const mongo = require('../lib/util/mongo')
 async function main() {
   await mongo.connect()
 
-  let counterBal = 0
-  let counterNum = 0
-  let counterTopo = 0
-  let counterVoie = 0
+  const modifiedBALs = await mongo.db.collection('bases_locales').updateMany(
+    {_deleted: {$exists: false}},
+    {$set: {_deleted: null}}
+  )
 
-  const balCursor = await mongo.db.collection('bases_locales').find({_deleted: {$exists: false}})
-  const numerosCursor = await mongo.db.collection('numeros').find({_deleted: {$exists: false}})
-  const toponymesCursor = await mongo.db.collection('toponymes').find({_deleted: {$exists: false}})
-  const voiesCursor = await mongo.db.collection('voies').find({_deleted: {$exists: false}})
+  const modifiedNumeros = await mongo.db.collection('numeros').updateMany(
+    {_deleted: {$exists: false}},
+    {$set: {_deleted: null}}
+  )
 
-  while (await balCursor.hasNext()) {
-    const baseLocale = await balCursor.next()
+  const modifiedToponymes = await mongo.db.collection('toponymes').updateMany(
+    {_deleted: {$exists: false}},
+    {$set: {_deleted: null}}
+  )
 
-    await mongo.db.collection('bases_locales').updateOne(
-      {_id: baseLocale._id},
-      {$set: {_deleted: null}}
-    )
-
-    counterBal += 1
-  }
-
-  while (await numerosCursor.hasNext()) {
-    const numero = await numerosCursor.next()
-
-    await mongo.db.collection('numeros').updateOne(
-      {_id: numero._id},
-      {$set: {_deleted: null}}
-    )
-
-    counterNum += 1
-  }
-
-  while (await toponymesCursor.hasNext()) {
-    const toponyme = await toponymesCursor.next()
-
-    await mongo.db.collection('toponymes').updateOne(
-      {_id: toponyme._id},
-      {$set: {_deleted: null}}
-    )
-
-    counterTopo += 1
-  }
-
-  while (await voiesCursor.hasNext()) {
-    const voie = await voiesCursor.next()
-
-    await mongo.db.collection('voies').updateOne(
-      {_id: voie._id},
-      {$set: {_deleted: null}}
-    )
-
-    counterVoie += 1
-  }
+  const modifiedVoies = await mongo.db.collection('voies').updateMany(
+    {_deleted: {$exists: false}},
+    {$set: {_deleted: null}}
+  )
 
   console.log('\nTerminé !\n')
   console.log('************************************\n')
-  console.log(` ${counterBal} BAL ont été modifiées \n`)
-  console.log(` ${counterNum} numéros ont été modifiés \n`)
-  console.log(` ${counterTopo} toponymes ont été modifiés \n`)
-  console.log(` ${counterVoie} voies ont été modifiées \n`)
+  console.log(` ${modifiedBALs.modifiedCount} BAL ont été modifiées \n`)
+  console.log(` ${modifiedNumeros.modifiedCount} numéros ont été modifiés \n`)
+  console.log(` ${modifiedToponymes.modifiedCount} toponymes ont été modifiés \n`)
+  console.log(` ${modifiedVoies.modifiedCount} voies ont été modifiées \n`)
   console.log('************************************\n')
 
   await mongo.disconnect()
