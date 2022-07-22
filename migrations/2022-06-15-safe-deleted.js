a0a35a749015cdc282cba215a3671da8f17df065
gllmr <guillaume@livingdata.co>
2022-07-22 11:31:38 +0200

    Remove bal content from soft-delete migration script

--- migrations/2022-06-15-safe-deleted.js
+++ migrations/2022-06-15-safe-deleted.js
@@ -11,27 +11,9 @@ async function main() {
     {$set: {_deleted: null}}
   )
 
-  const modifiedNumeros = await mongo.db.collection('numeros').updateMany(
-    {_deleted: {$exists: false}},
-    {$set: {_deleted: null}}
-  )
-
-  const modifiedToponymes = await mongo.db.collection('toponymes').updateMany(
-    {_deleted: {$exists: false}},
-    {$set: {_deleted: null}}
-  )
-
-  const modifiedVoies = await mongo.db.collection('voies').updateMany(
-    {_deleted: {$exists: false}},
-    {$set: {_deleted: null}}
-  )
-
   console.log('\nTerminé !\n')
   console.log('************************************\n')
   console.log(` ${modifiedBALs.modifiedCount} BAL ont été modifiées \n`)
-  console.log(` ${modifiedNumeros.modifiedCount} numéros ont été modifiés \n`)
-  console.log(` ${modifiedToponymes.modifiedCount} toponymes ont été modifiés \n`)
-  console.log(` ${modifiedVoies.modifiedCount} voies ont été modifiées \n`)
   console.log('************************************\n')
 
   await mongo.disconnect()
