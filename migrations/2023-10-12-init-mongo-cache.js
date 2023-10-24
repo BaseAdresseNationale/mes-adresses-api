fb7f96033d2bc23df832e91e9909dbf18679de0b
Guillaume Fay <guillaume_fay@hotmail.com>
2023-10-24 14:44:29 +0200

    rename tasks cache to mongo cache

--- migrations/2023-10-12-init-mongo-cache.js
+++ migrations/2023-10-12-init-mongo-cache.js
@@ -3,13 +3,13 @@
 require('dotenv').config()
 const {sub} = require('date-fns')
 const mongo = require('../lib/util/mongo')
-const {setTasksCache} = require('../lib/tasks/tasks-cache')
+const {setMongoCache} = require('../lib/tasks/mongo-cache')
 
 async function main() {
   await mongo.connect()
 
   const initCacheTs = sub(new Date(), {hours: 1})
-  await setTasksCache('detectConflictPublishedSince', initCacheTs)
+  await setMongoCache('detectConflictPublishedSince', initCacheTs)
 
   await mongo.disconnect()
   process.exit()
