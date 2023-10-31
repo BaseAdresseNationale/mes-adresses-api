98278c8003315b5785825502533efef5bd3b2699
Guillaume Fay <guillaume_fay@hotmail.com>
2023-10-31 10:05:32 +0100

    fix: avoid race condition in conflict detection (#415)
    * fix: avoid race condition in conflict detection
    
    * fix: add a script to init timestamp
    
    * fix: add a task queue to avoid concurrency
    
    * rename tasks cache to mongo cache

--- cron.js
+++ cron.js
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
 require('dotenv').config()
+const {CronJob} = require('cron')
 const ms = require('ms')
 const {detectOutdated, detectConflict, syncOutdated} = require('./lib/sync')
 const {removeSoftDeletedBALsOlderThanOneYear, removeDemoBALsOlderThanAMonth} = require('./lib/models/base-locale')
+const {TaskQueue} = require('./lib/tasks/task-queue')
 const mongo = require('./lib/util/mongo')
 
-const jobs = [
+const jobHandler = async job => {
+  console.log(`${new Date().toISOString().slice(0, 19)} | Starting job : ${job.name}`)
+  await job.handler()
+  console.log(`${new Date().toISOString().slice(0, 19)} | Ending job : ${job.name}`)
+}
+
+const queue = new TaskQueue()
+
+const tasks = [
   {
     name: 'detect outdated sync',
     every: '30s',
@@ -27,35 +37,59 @@ const jobs = [
       await syncOutdated()
     }
   },
+]
+
+const cronJobs = [
   {
+    // Purge soft deleted BAL for more than one year every day at 3:00 AM
     name: 'purge old deleted BALs',
-    every: '1h',
+    schedule: '0 2 * * *',
     async handler() {
       await removeSoftDeletedBALsOlderThanOneYear()
     }
   },
   {
+    // Purge old demo BAL every day at 2:00 AM
     name: 'purge demo BALs',
-    every: '24h',
+    schedule: '0 3 * * *',
     async handler() {
       await removeDemoBALsOlderThanAMonth()
     }
   }
 ]
 
+const launchTasks = () => {
+  tasks.forEach(task => {
+    setInterval(() => {
+      queue.pushTask(() => jobHandler(task))
+    }, ms(task.every))
+  })
+}
+
+const launchCronJobs = () => {
+  cronJobs.forEach(job => {
+    const cronJob = new CronJob(
+      job.schedule,
+      () => {
+        queue.pushTask(() => jobHandler(job))
+      },
+      false,
+      'Europe/Paris'
+    )
+
+    cronJob.start()
+  })
+}
+
 async function main() {
   await mongo.connect()
 
-  jobs.forEach(job => {
-    setInterval(() => {
-      const now = new Date()
-      console.log(`${now.toISOString().slice(0, 19)} | running job : ${job.name}`)
-      job.handler()
-    }, ms(job.every))
-  })
+  launchTasks()
+  launchCronJobs()
 }
 
 main().catch(error => {
   console.error(error)
   process.exit(1)
 })
+
