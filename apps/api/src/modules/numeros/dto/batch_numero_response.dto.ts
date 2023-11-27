2b075b22f0434d392cc0e572337c468f8333e580
Fufeck <fabien.tafforeau@gmail.com>
2023-11-27 17:11:40 +0400

    correct

--- apps/api/src/modules/numeros/dto/batch_numero_response.dto.ts
+++ apps/api/src/modules/numeros/dto/batch_numero_response.dto.ts
@@ -2,13 +2,12 @@ import { ApiProperty } from '@nestjs/swagger';
 import { UpdateBatchNumeroChangeDTO } from './update_batch_numero_change.dto';
 
 export class BatchNumeroResponseDTO {
-  @ApiProperty({ type: Number, required: true, nullable: true })
+  @ApiProperty({ type: Number, required: true })
   modifiedCount?: number;
 
   @ApiProperty({
     type: () => UpdateBatchNumeroChangeDTO,
     required: false,
-    nullable: true,
   })
   changes?: UpdateBatchNumeroChangeDTO;
 }
