aede87915f73c07074f8789446d650c854cf23da
Fufeck <fabien.tafforeau@gmail.com>
2024-02-14 17:15:26 +0100

    feat: add sensible field in route /search bal to super admin

--- apps/api/src/lib/utils/is-admin.utils.ts
+++ apps/api/src/lib/utils/is-admin.utils.ts
@@ -1,6 +1,10 @@
 import { CustomRequest } from '@/lib/types/request.type';
 import { BaseLocale } from '../../../../../libs/shared/src/schemas/base_locale/base_locale.schema';
 
+export function isSuperAdmin(req: CustomRequest) {
+  return req.headers.authorization === `Bearer ${process.env.ADMIN_TOKEN}`;
+}
+
 export function isAdmin(req: CustomRequest, baseLocale: BaseLocale) {
   return (
     req.headers.authorization === `Bearer ${baseLocale.token}` ||
