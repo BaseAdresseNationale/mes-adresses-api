d8529e6528ba60e4a957564aa40cf2ca957302c0
Fufeck <fabien.tafforeau@gmail.com>
2024-07-29 15:52:52 +0200

    fix: mailer mode dev

--- libs/shared/src/params/mailer.params.ts
+++ libs/shared/src/params/mailer.params.ts
@@ -5,25 +5,34 @@ import { join } from 'path';
 export const MailerParams = {
   imports: [ConfigModule],
   inject: [ConfigService],
-  useFactory: async (config: ConfigService) => ({
-    transport: {
-      host: config.get('SMTP_HOST'),
-      port: config.get('SMTP_PORT'),
-      secure: config.get('SMTP_SECURE') === 'YES',
-      auth: {
-        user: config.get('SMTP_USER'),
-        pass: config.get('SMTP_PASS'),
-      },
-    },
-    defaults: {
-      from: config.get('SMTP_FROM'),
-    },
-    template: {
-      dir: join(__dirname, '../../../', '/email-templates'),
-      adapter: new HandlebarsAdapter(),
-      options: {
-        strict: true,
-      },
-    },
-  }),
+  useFactory: async (config: ConfigService) =>
+    config.get('SMTP_HOST')
+      ? {
+          transport: {
+            host: config.get('SMTP_HOST'),
+            port: config.get('SMTP_PORT'),
+            secure: config.get('SMTP_SECURE') === 'YES',
+            auth: {
+              user: config.get('SMTP_USER'),
+              pass: config.get('SMTP_PASS'),
+            },
+          },
+          defaults: {
+            from: config.get('SMTP_FROM'),
+          },
+          template: {
+            dir: join(__dirname, '../../../', '/email-templates'),
+            adapter: new HandlebarsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        }
+      : {
+          transport: {
+            streamTransport: true,
+            newline: 'unix',
+            buffer: true,
+          },
+        },
 };
