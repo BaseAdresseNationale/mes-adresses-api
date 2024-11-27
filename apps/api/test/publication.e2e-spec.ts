b399bf92e3ed13ad1d7cb1970f03b01b3dd822f4
Fufeck <fabien.tafforeau@gmail.com>
2024-11-27 10:45:21 +0100

    correct test

--- apps/api/test/publication.e2e-spec.ts
+++ apps/api/test/publication.e2e-spec.ts
@@ -346,8 +346,8 @@ describe('PUBLICATION MODULE', () => {
 
       axiosMock.onPost(`/revisions/${revisionId}/compute`).reply(200, revision);
 
-      const csvFile = `cle_interop;id_ban_commune;id_ban_toponyme;id_ban_adresse;voie_nom;lieudit_complement_nom;numero;suffixe;certification_commune;commune_insee;commune_nom;position;long;lat;x;y;cad_parcelles;source;date_der_maj
-      91534_xxxx_00001_bis;${communeUuid};${toponymeUuid};${numeroUuid};rue de la paix;;1;bis;1;91534;Saclay;inconnue;8;42;1114835.92;6113076.85;;ban;2000-01-01`;
+      const csvFile = `cle_interop;id_ban_commune;id_ban_toponyme;id_ban_adresse;voie_nom;lieudit_complement_nom;numero;suffixe;certification_commune;commune_insee;commune_nom;commune_deleguee_insee;commune_deleguee_nom;position;long;lat;x;y;cad_parcelles;source;date_der_maj
+      91534_xxxx_00001_bis;${communeUuid};${toponymeUuid};${numeroUuid};rue de la paix;;1;bis;1;91534;Saclay;;;inconnue;8;42;1114835.92;6113076.85;;ban;2000-01-01`;
       axiosMock
         .onPut(`/revisions/${revisionId}/files/bal`)
         .reply(({ data }) => {
@@ -410,7 +410,7 @@ describe('PUBLICATION MODULE', () => {
         files: [
           {
             type: 'bal',
-            hash: '0c5d808a7e5612c9467607c574cb2317a76fe04d493efbd61b55a31bbd194227',
+            hash: 'a512238a358a47446e4eb6b89dbfbecc03b81fcb2492eed54e277699d1c49c62',
           },
         ],
       };
