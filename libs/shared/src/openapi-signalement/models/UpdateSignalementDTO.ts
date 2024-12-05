b8fdbc0f413da922a6fc9c2f25fa3bc87cb7afe2
Guillaume Fay <guillaume_fay@hotmail.com>
2024-12-05 16:25:46 +0100

    feat: add signalement module

---
+++ libs/shared/src/openapi-signalement/models/UpdateSignalementDTO.ts
@@ -0,0 +1,16 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type UpdateSignalementDTO = {
+    status: UpdateSignalementDTO.status;
+};
+export namespace UpdateSignalementDTO {
+    export enum status {
+        PENDING = 'PENDING',
+        IGNORED = 'IGNORED',
+        PROCESSED = 'PROCESSED',
+        EXPIRED = 'EXPIRED',
+    }
+}
+
