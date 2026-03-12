485e62aea59ae23d7ff518087e114a5e1def74ea
Guillaume Fay <guillaume_fay@hotmail.com>
2026-03-12 17:53:52 +0100

    fix: update dto

--- apps/api/src/modules/signalement/dto/update-signalement-dto.ts
+++ apps/api/src/modules/signalement/dto/update-signalement-dto.ts
@@ -1,6 +1,36 @@
-import { Report } from '@/shared/openapi-signalement';
+import { MissingAddressContext, Report } from '@/shared/openapi-signalement';
 import { ApiProperty } from '@nestjs/swagger';
-import { ArrayMinSize, IsEnum } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsEnum,
+  IsObject,
+  IsOptional,
+  ValidateNested,
+} from 'class-validator';
+
+class MissingAddressContextDTO implements MissingAddressContext {
+  @ApiProperty({ required: false, nullable: true, type: String })
+  idBAN?: string;
+
+  @ApiProperty({ required: false, nullable: true, type: String })
+  idRNB?: string;
+}
+
+export class UpdateOneReportDTO {
+  @ApiProperty({ required: true, nullable: false, enum: Report.status })
+  @IsEnum(Report.status)
+  status: Report.status;
+
+  @ApiProperty({ required: false, nullable: true, type: String })
+  rejectionReason?: string;
+
+  @IsOptional()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => MissingAddressContextDTO)
+  context?: MissingAddressContextDTO | null;
+}
 
 export class UpdateManyReportsDTO {
   @ApiProperty({
--- libs/shared/src/openapi-signalement/models/MissingAddressContext.ts
+++ libs/shared/src/openapi-signalement/models/MissingAddressContext.ts
@@ -4,6 +4,6 @@
 /* eslint-disable */
 export type MissingAddressContext = {
     idRNB?: string | null;
-    banId?: string | null;
+    idBAN?: string | null;
 };
 
