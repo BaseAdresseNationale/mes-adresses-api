314d6dc36c3a3c041579a0a1ea3145eb7f4d9830
Fabien Tafforeau <fabien.tafforeau@gmail.com>
2026-05-13 15:05:20 +0200

    feat: delete DEMO BAL over one month (#575)

--- apps/cron/src/modules/task/tasks/remove_demo_bal.task.ts
+++ apps/cron/src/modules/task/tasks/remove_demo_bal.task.ts
@@ -18,10 +18,10 @@ export class RemoveDemoBalTask implements Task {
   ) {}
 
   public async run() {
-    // On créer le where pour selectioné toutes les BALs DEMO qui on plus d'un mois
-    const deleteTime = subMonths(new Date(), 1);
+    // On créer le where pour selectioné toutes les BALs DEMO qui ont été créé il y a plus d'un mois
+    const createdTime = subMonths(new Date(), 1);
     const where: FindOptionsWhere<BaseLocale> = {
-      deletedAt: LessThan(deleteTime),
+      createdAt: LessThan(createdTime),
       status: StatusBaseLocalEnum.DEMO,
     };
     // On supprime les BAL, et par CASCADE les voie, toponymes, numeros et positions sont supprimé également
