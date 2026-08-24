4fc251b32c361e39460547e0d1f800c85131e9c1
Fufeck <fabien.tafforeau@gmail.com>
2026-08-24 10:17:18 +0200

    refacto cron: delete task useless

--- apps/cron/src/modules/task/task.module.ts
+++ apps/cron/src/modules/task/task.module.ts
@@ -11,14 +11,10 @@ import { CacheModule } from '@/shared/modules/cache/cache.module';
 import { S3Module } from '@/shared/modules/s3/s3.module';
 
 import { TaskProcessor } from './task.processor';
-import { DetectOutdatedTask } from './tasks/detect_outdated.task';
-import { SyncOutdatedTask } from './tasks/sync_outdated.task';
-import { DetectConflictTask } from './tasks/detect_conflict.task';
 import { RemoveSoftDeleteBalTask } from './tasks/remove_soft_delete_bal.task';
 import { RemoveDemoBalTask } from './tasks/remove_demo_bal.task';
 import { UploadTracesTask } from './tasks/upload_traces.task';
 import { QUEUE_NAME } from '@/shared/params/queue_name.const';
-import { ForcePublishTask } from './tasks/force_publish.task';
 import { ResetCommunesForWebinaireTask } from './tasks/reset_communes_for_webinaire.task';
 
 @Module({
@@ -40,13 +36,9 @@ import { ResetCommunesForWebinaireTask } from './tasks/reset_communes_for_webina
   providers: [
     Logger,
     TaskProcessor,
-    DetectOutdatedTask,
-    SyncOutdatedTask,
-    DetectConflictTask,
     RemoveSoftDeleteBalTask,
     RemoveDemoBalTask,
     UploadTracesTask,
-    ForcePublishTask,
     ResetCommunesForWebinaireTask,
   ],
 })
--- apps/cron/src/modules/task/task.processor.ts
+++ apps/cron/src/modules/task/task.processor.ts
@@ -3,25 +3,17 @@ import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 import { TaskTitle } from '@/shared/types/task.type';
 import { QUEUE_NAME } from '@/shared/params/queue_name.const';
-import { DetectOutdatedTask } from './tasks/detect_outdated.task';
-import { DetectConflictTask } from './tasks/detect_conflict.task';
-import { SyncOutdatedTask } from './tasks/sync_outdated.task';
 import { RemoveSoftDeleteBalTask } from './tasks/remove_soft_delete_bal.task';
 import { RemoveDemoBalTask } from './tasks/remove_demo_bal.task';
 import { UploadTracesTask } from './tasks/upload_traces.task';
-import { ForcePublishTask } from './tasks/force_publish.task';
 import { ResetCommunesForWebinaireTask } from './tasks/reset_communes_for_webinaire.task';
 
 @Processor(QUEUE_NAME)
 export class TaskProcessor extends WorkerHost {
   constructor(
-    private readonly detectOutdatedTask: DetectOutdatedTask,
-    private readonly detectConflictTask: DetectConflictTask,
-    private readonly syncOutdatedTask: SyncOutdatedTask,
     private readonly removeSoftDeleteBalTask: RemoveSoftDeleteBalTask,
     private readonly removeDemoBalTask: RemoveDemoBalTask,
     private readonly uploadTracesTask: UploadTracesTask,
-    private readonly forcePublishTask: ForcePublishTask,
     private readonly resetCommunesForWebinaireTask: ResetCommunesForWebinaireTask,
   ) {
     super();
@@ -34,18 +26,6 @@ export class TaskProcessor extends WorkerHost {
     );
     try {
       switch (job.name) {
-        // case TaskTitle.DETECT_OUTDATED:
-        //   await this.detectOutdatedTask.run();
-        //   break;
-        // case TaskTitle.DETECT_CONFLICT:
-        //   await this.detectConflictTask.run();
-        //   break;
-        // case TaskTitle.SYNC_OUTDATED:
-        //   await this.syncOutdatedTask.run();
-        //   break;
-        case TaskTitle.FORCE_PUBLISH:
-          await this.forcePublishTask.run(job.data.balId);
-          break;
         case TaskTitle.REMOVE_SOFT_DELETE_BAL:
           await this.removeSoftDeleteBalTask.run();
           break;
